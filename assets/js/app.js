761391cfbe04219d9791d5bbf8954815b61fea7f
Andrey Tokarev <atokarev1@gmail.com>
2021-03-15 15:44:01 -0500

    added scatter plot

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,15 +15,87 @@ risk_data.forEach(function(data) {
     data.abbr = data.abbr;
     data.income = +data.income;
 
-console.log(data.age);
-console.log(data.smokes);
-console.log(data.healthcare);
-console.log(data.poverty);
-console.log(data.abbr);
-console.log(data.income);
+    console.log(data.age);
+    console.log(data.smokes);
+    console.log(data.healthcare);
+    console.log(data.poverty);
+    console.log(data.abbr);
+    console.log(data.income);
 
     });
+    
+    let svg_width = 950;
+    let svg_height = 600;
+    
+    let margin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left:30
+    };
 
+// set margins
+    let width = svg_width - margin.left - margin.right;
+    let height = svg_height - margin.top - margin.bottom;
+
+
+// add svg tag to html
+let svg = d3.select("#scatter")
+.append("svg")
+.attr("width", svg_width)
+.attr("height", svg_height);
+
+let scatter_chart = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//Scales for the graph
+let xLinearScale = d3.scaleLinear()
+.domain([8.5, d3.max(risk_data, d => d.poverty)])
+.range([0, width]);
+
+let yLinearScale = d3.scaleLinear()
+.domain([3.5, d3.max(risk_data, d => d.healthcare)])
+.range([height, 0]);
+
+//Create axis (x and y)
+    let xAxis = d3.axisBottom(xLinearScale);
+    let yAxis = d3.axisLeft(yLinearScale);
+
+//Append x and y axis
+scatter_chart.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis);
+
+scatter_chart.append("g")
+    .call(yAxis);
+
+//add circles to the graph
+    scatter_chart.selectAll("circle")
+        .data(risk_data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", 18)
+        .attr("fill", "#6666FF")
+        .attr("opacity", ".6")
+        .attr("stroke-width", "2")
+        .attr("stroke", "blue");
+
+        scatter_chart.select("g")
+        .selectAll("circle")
+        .data(risk_data)
+        .enter()
+        .append("text")
+        .text(d => d.abbr)
+        .attr("x", d => xLinearScale(d.poverty))
+        .attr("y", d => yLinearScale(d.healthcare))
+        .attr("dy",-537)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "14px")
+        .attr("fill", "black");
+
+    
 
 });
 
